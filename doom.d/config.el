725c317de4c15ebac2ea4f6bc479ae1f5b1d19c9
Oliver Charles <ollie@ocharles.org.uk>
2018-07-02 09:13:06 +0100

    Lets give normal Iosevka a try for a while

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,6 +1,6 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-(set-frame-font "Iosevka Light 16" t t)
+(set-frame-font "Iosevka 16" t t)
 (setq +pretty-code-iosevka-ligatures-enabled-by-default t)
 (set-pretty-symbols! 'haskell-mode :iosevka t)
 
