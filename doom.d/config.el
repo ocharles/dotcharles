e52c25a515667633d6387aea73bf27ed7b5d618b
Oliver Charles <ollie@ocharles.org.uk>
2018-07-09 23:23:47 +0100

    Correctly enable Iosevka symbols

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,8 +1,6 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 (set-frame-font "Iosevka 16" t t)
-(setq +pretty-code-iosevka-ligatures-enabled-by-default t)
-(set-pretty-symbols! 'haskell-mode :iosevka t)
 
 (def-package! dhall-mode)
 (def-package! direnv)
