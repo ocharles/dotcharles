558b3fb4f25027dbf0a037ced3217649bb7cd266
Ollie Charles <ollie@ocharles.org.uk>
2022-12-05 14:25:53 +0000

    home-manager

---
+++ home-manager/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "My home-manager configuration";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations.ocharles = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./home.nix
+        ];
+      };
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+    };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,131 @@
+{ config, pkgs, ... }:
+
+let
+  iosevka = pkgs.iosevka.override {
+    set = "custom";
+    privateBuildPlan = {
+      family = "Iosevka Kitty";
+      spacing = "normal";
+      serifs = "sans";
+      no-cv-ss = false;
+      export-glyph-names = true;
+
+      ligations = {
+        "inherits" = "haskell";
+      };
+    };
+  };
+in
+
+{
+  home = {
+    username = "ollie";
+    homeDirectory = "/home/ollie";
+
+    packages = with pkgs; [
+      alloy5
+      ardour
+      fd
+      file
+      gdb
+      gh
+      git-crypt
+      gitAndTools.git-annex
+      gitAndTools.hub
+      guitarix
+      iosevka
+      kdeconnect
+      konsole
+      nix-diff
+      ntfs3g
+      okular
+      picard
+      qjackctl
+      restic
+      ripgrep
+      spectacle
+      sqlite
+      tlaplusToolbox
+      tokei
+      unzip
+      vlc
+      zotero
+    ];
+    stateVersion = "21.05";
+  };
+
+  fonts.fontconfig.enable = true;
+
+  programs = {
+    bat.enable = true;
+    command-not-found.enable = true;
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    emacs = {
+      enable = false;
+      extraPackages = epkgs: [ epkgs.vterm ];
+      overrides = self: super: {
+        emacs-libvterm =
+          super.emacs-libvterm.overrideAttrs (old: {
+            src = builtins.fetchGit https://github.com/akermu/emacs-libvterm;
+          });
+      };
+      package = pkgs.emacsGit;
+    };
+    firefox.enable = true;
+    fish.enable = true;
+    home-manager.enable = true;
+    htop.enable = true;
+    git = {
+      enable = true;
+      delta.enable = true;
+    };
+    jq.enable = true;
+    starship = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    kitty = {
+      enable = true;
+      font = { name = "Iosevka Kitty Medium"; package = iosevka; };
+      # font = { name = "Berkeley Mono Regular"; };
+
+      # include ${
+      #   pkgs.fetchurl {
+      #     url = https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/kitty_tokyonight_storm.conf;
+      #     sha256 = "0vidpk1ridlakz776pgz4lxkmq5xn75ghsr9a2krh9zlm1r264jh";
+      #   }
+      # }
+
+      extraConfig = ''
+        font_size 14.0
+        window_margin_width 3
+        include ${
+          pkgs.fetchurl {
+            url = https://raw.githubusercontent.com/catppuccin/kitty/main/macchiato.conf;
+            sha256 = "sha256-lk0ca7An8GSCbBMYTb/y37RogsidZRajTt12R+/z2Ls=";
+          }
+        }
+      '';
+    };
+  };
+
+  # xdg.configFile.nvim = {
+  #   source = /home/ollie/config/nvim;
+  #   recursive = true;
+  # };
+
+  wayland.windowManager.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+    config = {
+      input = {
+        "*" = {
+          xkb_layout = "dvorak";
+        };
+      };
+    };
+  };
+}
