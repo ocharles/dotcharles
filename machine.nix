d1f1da3e4f956fe169cda5ee89ab8763606e7c02
Ollie Charles <ollie@ocharles.org.uk>
2024-10-12 18:51:36 +0100

    Run `nix diff-closures` on machine activation
    From https://discourse.nixos.org/t/nvd-simple-nix-nixos-version-diff-tool/12397/39

--- machine.nix
+++ machine.nix
@@ -19,4 +19,10 @@
     extraGroups = [ "audio" "dialout" "realtime" "wheel" ];
     shell = pkgs.fish;
   };
+
+  system.activationScripts.diff = ''
+    if [[ -e /run/current-system ]]; then
+      ${pkgs.nix}/bin/nix store diff-closures /run/current-system "$systemConfig"
+    fi
+  '';
 } 
